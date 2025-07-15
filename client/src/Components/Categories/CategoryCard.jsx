521092879fbdefcd80ba0d16a9f10434117a52ea
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-07-15 21:07:53 +0530

    Creating Category Card And Highlighting Selected Category

---
+++ client/src/Components/Categories/CategoryCard.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import "../style.css"
+import { CategoryContext } from '../../context/CategoryContextProvider';
+
+const CategoryCard = ({ category }) => {
+	const {hotelCategory} = useContext(CategoryContext)
+	const isActive = hotelCategory === category;
+	const selectedClass = isActive ? "category-item category-item-selected" : "category-item"
+	return <div className={selectedClass}>{category}</div>
+};
+
+export default CategoryCard
