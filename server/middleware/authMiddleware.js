b71c63773895a4e3f08743e38f9a5a4a7fee25ae
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-07-22 03:47:36 +0530

    config: Production Enhancement

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,10 +1,20 @@
 const UserModel = require("../models/user.model");
 const { verifyToken } = require("../utils/jwtUtil");
+const { errorCreator } = require("../utils/responseCreator");
 
 const authMiddleware = async (req,res,next) => {
 	try {
+		console.log("Cookies on Incoming Requests: ",req.cookies)
 		const { authToken } = req.cookies;
+		if(!authToken){
+			console.error("authToken Missing In cookies!!!!")
+			errorCreator("authToken Missing in Cookies",401)
+		}
 		const data = verifyToken(authToken);
+		if(!data){
+			console.error("authToken Verify Error")
+			errorCreator("authToken Verify Error")
+		}
 		const { email } = data;
 		const { password, ...userData } = await UserModel.findUser(email);
 		res.locals.user = { email };
